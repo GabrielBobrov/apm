a04315c2c199b1b8df5eec8dd2170c9ca5babc9b
Gabriel Gomes Bobrov Lopes <gabriel.gomes@autotrac.com.br>
2022-07-01 13:53:45 -0300

    add data for table

--- src/app/products/product-list.componetn.ts
+++ src/app/products/product-list.componetn.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  products: any[] = [
+    {
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2021',
+      description: 'blablabla',
+      price: 32.88,
+      startRating: 4.2,
+      imageUrl: 'assets/images/gardem_cart.png',
+    },
+    {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TXB-0223',
+      releaseDate: 'March 18, 2021',
+      description: 'blablabla',
+      price: 8.88,
+      startRating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
+    },
+  ];
 }
