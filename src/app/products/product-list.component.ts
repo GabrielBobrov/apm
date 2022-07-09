8a285c1ff119eb186f8cc434e567474f2534be19
Gabriel Bobrov <bielbobrov@hotmail.com>
2022-07-09 13:54:00 -0300

    add filter

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -21,10 +21,10 @@ export class ProductListComponent implements OnInit {
   set listFilter(value: string) {
     this._listFilter = value;
     console.log('In Setter', value);
-    this.filteredProducts 
+    this.filteredProducts = this.performFilter(value);
   }
 
-  filteredProducts: IProduct[] = []
+  filteredProducts: IProduct[] = [];
   products: IProduct[] = [
     {
       productId: 2,
@@ -47,6 +47,13 @@ export class ProductListComponent implements OnInit {
       imageUrl: 'assets/images/hammer.png',
     },
   ];
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
+  }
 
   ngOnInit(): void {
     this.listFilter = 'cart';
